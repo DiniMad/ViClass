0ef9610c382c12bb8ee2d11d3f20e84c88aeb91e
DiniMad <payoff900@gmail.com>
2019-05-30 09:32:06 +0430

    Revert "The Class model and the ClassStudent model added to the project models."
    This reverts commit 3b34e81e530cfd18dccb03856b402df7548de4d2.

--- ViClass/Models/ApplicationUser.cs
+++ ViClass/Models/ApplicationUser.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +8,5 @@ namespace ViClass.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public ApplicationUser()
-        {
-            ClassAsStudent=new Collection<ClassStudent>();
-        }
-        public Class ClassAsInstructor{ get; set; }
-        public ICollection<ClassStudent> ClassAsStudent { get; set; }
     }
 }
