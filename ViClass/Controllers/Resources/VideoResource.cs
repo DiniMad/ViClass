6b3486f2c82b8a58b6f15e24be1488bab8be7eeb
DiniMad <payoff900@gmail.com>
2019-06-04 20:30:20 +0430

    VideoResource class created in resource folder.

---
+++ ViClass/Controllers/Resources/VideoResource.cs
@@ -0,0 +1,11 @@
+﻿namespace ViClass.Controllers.Resources
+{
+    public class VideoResource
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public uint VolumeInMg { get; set; }
+        public string LengthFormatted { get; set; }
+    }
+}
