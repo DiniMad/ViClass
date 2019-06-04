f81d066d7e72f3eb55aa8b006516748b8c3fa758
DiniMad <payoff900@gmail.com>
2019-06-04 20:30:59 +0430

    SharedFileResource class created in resource folder.

---
+++ ViClass/Controllers/Resources/SharedFileResource.cs
@@ -0,0 +1,10 @@
+﻿namespace ViClass.Controllers.Resources
+{
+    public class SharedFileResource
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public ushort VolumeInMg { get; set; }
+    }
+}
