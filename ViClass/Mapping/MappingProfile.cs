c46b62e15903e2b9e5c10b942987e6e0bc915ed3
DiniMad <payoff900@gmail.com>
2019-06-04 22:00:04 +0430

    A mapping profile created for AutoMapper.

---
+++ ViClass/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ViClass.Controllers.Resources;
+using ViClass.Models;
+
+namespace ViClass.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Class, ClassResource>();
+            CreateMap<Video, VideoResource>();
+            CreateMap<SharedFile, SharedFileResource>();
+            CreateMap<WeekTimeSchedule, WeekTimeScheduleResource>();
+            //CreateMap<ClassStudent, ClassStudentResource>();
+        }
+    }
+}
