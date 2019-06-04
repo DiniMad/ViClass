670f767d1619b22890fffe09221c17f43854198a
DiniMad <payoff900@gmail.com>
2019-06-04 20:52:00 +0430

    Class controller with Classes action added to the project.

---
+++ ViClass/Controllers/ClassController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViClass.Data;
+using ViClass.Models;
+
+namespace ViClass.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClassController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Class> Classes()
+        {
+            return _context.Classes
+                           .Include(c=>c.Instructor)
+                           .Include(c=>c.WeekTimeSchedule)
+                           .Include(c=>c.Videos);
+        }
+    }
+}
