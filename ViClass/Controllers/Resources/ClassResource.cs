0d49e161ea465fa5dce8e554e015e7f7694c544e
DiniMad <payoff900@gmail.com>
2019-06-06 16:17:28 +0430

    The property DayOfWeekSchedules in ClassResource class changed to accept only values with count equal to 7

--- ViClass/Controllers/Resources/ClassResource.cs
+++ ViClass/Controllers/Resources/ClassResource.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using ViClass.Models;
 
@@ -15,7 +16,20 @@ namespace ViClass.Controllers.Resources
         public bool ShouldPresentVideo { get; set; }
         public ICollection<VideoResource> Videos { get; set; }
         public ICollection<SharedFileResource> SharedFiles { get; set; }
-        public ICollection<DayOfWeekScheduleResource> DayOfWeekSchedules { get; set; }
+        public ICollection<DayOfWeekScheduleResource> DayOfWeekSchedules
+        {
+            get => DayOfWeekSchedules;
+            set
+            {
+                if (value.Count >= 7)
+                {
+                    //Log
+                    Console.WriteLine("The Property '{0}' can not have more than 7 values.",nameof(DayOfWeekSchedules));
+                    return;
+                }
+                DayOfWeekSchedules = value;
+            }
+        }
         public string StartDateFormatted { get; set; }
         public string EndDateFormatted { get; set; }
         public byte PeriodInEveryXWeeks { get; set; }
