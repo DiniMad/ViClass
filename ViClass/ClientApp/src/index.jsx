f6558b0f653ef0b361f1ddf8758b7e48412371ab
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-08-21 03:47:05 +0430

    CSS file genereted based on SASS file moved to src/css folder and project reconfiged to import the CSS file.

--- ViClass/ClientApp/src/index.jsx
+++ ViClass/ClientApp/src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import "./style.min.css"
+import "./css/style.min.css"
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
