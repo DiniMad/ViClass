387d81ec98d901dbb530741c26329883cec2fc58
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-09-22 23:24:56 +0330

    Re-Link the js file in _Layout.cshtml and some constraint for an input.
    A function created to prevent user from using '-' character or incrementing or decrementing value of "Input_StudentNumber" field using arrow keys.
    Also src attribute of script tag in _Layout.cshtml changed to point to WWW folder.

--- ViClass/wwwroot/js/site.js
+++ ViClass/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Subscribe to input with id "Input_StudentNumber" onkeydown event.
+var studentNumberElement = document.getElementById("Input_StudentNumber")
+if (studentNumberElement)
+    studentNumberElement.onkeydown = function(event) {
+        onNumberInputKeyDown(event);
+    };
+
+// Prevent user from using '-' character or incrementing or decrementing value using arrow keys.
+function onNumberInputKeyDown(event) {
+    var x = event.which || event.keyCode;
+    if (x == 38 || x == 40 || x == 189) event.preventDefault();
+}
+
+// // Subscribe to input with id "Input_StudentNumber" onpaste event.
+// document.getElementById("Input_StudentNumber").onpaste = function(event) {
+//     onNumberInputPaste(event);
+// };
+
+// // Prevent user from pasing '-' character.
+// function onNumberInputPaste(event) {
+//     // event.target.value=event.clipboardData.getData("Text").replace('-','');
+//     console.log(event.target);
+// }
