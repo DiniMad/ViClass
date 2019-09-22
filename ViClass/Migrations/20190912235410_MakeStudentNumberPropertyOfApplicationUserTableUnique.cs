82f17dee459e5f31d4a2992906914f857a564147
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-09-22 23:52:46 +0330

    It's a migration to make StudentNumber field unique.

---
+++ ViClass/Migrations/20190912235410_MakeStudentNumberPropertyOfApplicationUserTableUnique.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class MakeStudentNumberPropertyOfApplicationUserTableUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_StudentNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_StudentNumber",
+                table: "AspNetUsers",
+                column: "StudentNumber",
+                unique: true,
+                filter: "[StudentNumber] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_StudentNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_StudentNumber",
+                table: "AspNetUsers",
+                column: "StudentNumber");
+        }
+    }
+}
