d0323480d0fac8ab4c546f7a3a1655cb9a98a3e8
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-09-23 00:38:04 +0330

    Login changed to show loading page on Login and Login callback happened.

--- ViClass/ClientApp/src/components/api-authorization/Login.jsx
+++ ViClass/ClientApp/src/components/api-authorization/Login.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { Component } from 'react';
-import authService from './AuthorizeService';
-import { AuthenticationResultStatus } from './AuthorizeService';
-import { LoginActions, QueryParameterNames, ApplicationPaths } from './ApiAuthorizationConstants';
+import React from "react";
+import { Component } from "react";
+import authService from "./AuthorizeService";
+import { AuthenticationResultStatus } from "./AuthorizeService";
+import {
+    LoginActions,
+    QueryParameterNames,
+    ApplicationPaths
+} from "./ApiAuthorizationConstants";
+import Loading from "../Loading";
 
 // The main responsibility of this component is to handle the user's login process.
 // This is the starting point for the login process. Any component that needs to authenticate
@@ -47,16 +52,16 @@ export class Login extends Component {
         const { message } = this.state;
 
         if (!!message) {
-            return <div>{message}</div>
+            return <div>{message}</div>;
         } else {
             switch (action) {
                 case LoginActions.Login:
-                    return (<div>Processing login</div>);
+                    return <Loading></Loading>;
                 case LoginActions.LoginCallback:
-                    return (<div>Processing login callback</div>);
+                    return <Loading></Loading>;
                 case LoginActions.Profile:
                 case LoginActions.Register:
-                    return (<div></div>);
+                    return <div></div>;
                 default:
                     throw new Error(`Invalid action '${action}'`);
             }
@@ -91,7 +96,7 @@ export class Login extends Component {
             case AuthenticationResultStatus.Redirect:
                 // There should not be any redirects as the only time completeSignIn finishes
                 // is when we are doing a redirect sign in flow.
-                throw new Error('Should not redirect.');
+                throw new Error("Should not redirect.");
             case AuthenticationResultStatus.Success:
                 await this.navigateToReturnUrl(this.getReturnUrl(result.state));
                 break;
@@ -99,7 +104,9 @@ export class Login extends Component {
                 this.setState({ message: result.message });
                 break;
             default:
-                throw new Error(`Invalid authentication result status '${result.status}'.`);
+                throw new Error(
+                    `Invalid authentication result status '${result.status}'.`
+                );
         }
     }
 
@@ -108,17 +115,29 @@ export class Login extends Component {
         const fromQuery = params.get(QueryParameterNames.ReturnUrl);
         if (fromQuery && !fromQuery.startsWith(`${window.location.origin}/`)) {
             // This is an extra check to prevent open redirects.
-            throw new Error("Invalid return url. The return url needs to have the same origin as the current page.")
+            throw new Error(
+                "Invalid return url. The return url needs to have the same origin as the current page."
+            );
         }
-        return (state && state.returnUrl) || fromQuery || `${window.location.origin}/`;
+        return (
+            (state && state.returnUrl) ||
+            fromQuery ||
+            `${window.location.origin}/`
+        );
     }
 
     redirectToRegister() {
-        this.redirectToApiAuthorizationPath(`${ApplicationPaths.IdentityRegisterPath}?${QueryParameterNames.ReturnUrl}=${encodeURI(ApplicationPaths.Login)}`);
+        this.redirectToApiAuthorizationPath(
+            `${ApplicationPaths.IdentityRegisterPath}?${
+                QueryParameterNames.ReturnUrl
+            }=${encodeURI(ApplicationPaths.Login)}`
+        );
     }
 
     redirectToProfile() {
-        this.redirectToApiAuthorizationPath(ApplicationPaths.IdentityManagePath);
+        this.redirectToApiAuthorizationPath(
+            ApplicationPaths.IdentityManagePath
+        );
     }
 
     redirectToApiAuthorizationPath(apiAuthorizationPath) {
