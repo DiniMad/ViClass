4fdcdc968f4b61520c8aa93d8b09d2169803d8dd
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-09-23 00:19:34 +0330

    Loading component added to the project.

---
+++ ViClass/ClientApp/src/components/Loading.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import authService from "./api-authorization/AuthorizeService";
+import "../css/loading.css";
+
+class Loading extends Component {
+    componentDidUpdate = async () => {
+        const url = window.location.href;
+        const result = await authService.completeSignIn(url);
+        if (result.status !== "success")
+            console.log("Error: " + result.message);
+    };
+    render() {
+        return (
+            <div id="loader-wrapper">
+                <div id="loader"></div>
+
+                <div className="loader-section section-left"></div>
+                <div className="loader-section section-right"></div>
+            </div>
+        );
+    }
+}
+export default Loading;
