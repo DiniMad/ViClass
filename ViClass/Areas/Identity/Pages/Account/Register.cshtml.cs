1d028580e89e698e0abcbfd18b1f7855757e6335
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-09-23 15:57:11 +0330

    Register page and code behind refactored.

--- ViClass/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ViClass/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -21,61 +22,74 @@ namespace ViClass.Areas.Identity.Pages.Account
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+        private readonly UserManager<ApplicationUser>   _userManager;
+        private readonly ILogger<RegisterModel>         _logger;
+        private readonly IEmailSender                   _emailSender;
 
         public RegisterModel(
-            UserManager<ApplicationUser> userManager,
+            UserManager<ApplicationUser>   userManager,
             SignInManager<ApplicationUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel>         logger,
+            IEmailSender                   emailSender)
         {
-            _userManager = userManager;
+            _userManager   = userManager;
             _signInManager = signInManager;
-            _logger = logger;
-            _emailSender = emailSender;
+            _logger        = logger;
+            _emailSender   = emailSender;
         }
 
-        [BindProperty]
-        public InputModel Input { get; set; }
+        [BindProperty] public InputModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        public class InputModel
+        public class InputModel : IValidatableObject
         {
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
+            [Required(ErrorMessage     = "ایمیل را وارد کنید.")]
+            [EmailAddress(ErrorMessage = "ایمیل وارد شده صحیح نیست.")]
+            [Display(Name              = "ایمیل")]
             public string Email { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "شماره دانشجویی را وارد کنید.")]
+            [Display(Name          = "شماره دانشجویی")]
+            [Range(typeof(ulong), "9000000000", "9999999999", ErrorMessage =
+                "شماره دانشجویی یک عدد بدون علامت 10 رقمی است.")]
+            public ulong StudentNumber { get; set; }
+
+            [Required(ErrorMessage          = "رمز عبور را وارد کنید.")]
+            [StringLength(100, ErrorMessage = "{0} باید حداقل {2} و حداکثر {1} کارکتر باشد.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "رمز عبور")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name                     = "تکرار رمز عبور")]
+            [Compare(nameof(Password), ErrorMessage = "رمز عبور و تکرار رمز عبور باید یکسان باشند.")]
             public string ConfirmPassword { get; set; }
+
+            public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+            {
+                if (!Regex.IsMatch(Password, @"^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$"))
+                    yield return new ValidationResult(
+             "پسورد باید 6 کارکتر و شامل حداقل یک عدد, یک حرف کوچک, و یک حرف بزرگ باشد.", new []{nameof(Password)});
+            }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            ReturnUrl = returnUrl;
+            ReturnUrl      = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl      = returnUrl ?? Url.Content("~/dashboard");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser
+                    {UserName = Input.Email, Email = Input.Email, StudentNumber = Input.StudentNumber.ToString()};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
@@ -84,17 +98,17 @@ namespace ViClass.Areas.Identity.Pages.Account
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+                                               "/Account/ConfirmEmail",
+                                               pageHandler: null,
+                                               values: new {area = "Identity", userId = user.Id, code = code},
+                                               protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                                                      $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                        return RedirectToPage("RegisterConfirmation", new {email = Input.Email});
                     }
                     else
                     {
@@ -102,6 +116,7 @@ namespace ViClass.Areas.Identity.Pages.Account
                         return LocalRedirect(returnUrl);
                     }
                 }
+
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
