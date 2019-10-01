9157aef5593957050c4f0c04d3e3d2f74c83253d
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-01 15:29:17 +0330

    UserController created to work around user model.

---
+++ ViClass/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViClass.Controllers.Resources;
+using ViClass.Data;
+using ViClass.Models;
+
+namespace ViClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper              _mapper;
+
+        public UserController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper  = mapper;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUserResource>> GetUser(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<ApplicationUser, ApplicationUserResource>(user);
+        }
+    }
+}
