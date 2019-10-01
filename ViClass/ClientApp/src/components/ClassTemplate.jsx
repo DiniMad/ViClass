54cbf1b775b88835075b19be77e84d413d5b28e7
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-01 22:16:11 +0330

    A method created to summarize text in ClassTemplate component.
    A method  summarize and another one to get index of character created and used both together in ClassTemplate component.

--- ViClass/ClientApp/src/components/ClassTemplate.jsx
+++ ViClass/ClientApp/src/components/ClassTemplate.jsx
@@ -15,10 +15,10 @@ class ClassTemplate extends Component {
                     backgroundImage: `linear-gradient(to bottom right,hsl(${firstRandomNumber}, ${hslColorConf}),hsl(${secendRandomNumber}, ${hslColorConf})`
                 }}
             >
-                <h3>{this.props.title}</h3>
-                <p>{this.props.description}</p>
+                <h3>{this.summarizeText(this.props.title, 30)}</h3>
+                <p>{this.summarizeText(this.props.description, 180)}</p>
                 <a href={`/api/user/${this.props.instructor.id}`}>
-                    {this.props.instructor.userName}
+                    {this.summarizeText(this.props.instructor.userName, 30)}
                 </a>
                 {this.renderClassDate(
                     this.props.dayOfWeekSchedules,
@@ -26,6 +26,41 @@ class ClassTemplate extends Component {
                 )}
             </div>
         );
+    }
+    summarizeText(text, length) {
+        // Check if every line has atleast one white space (for Description field).
+        if (text.length < length) {
+            let indexOfWhiteSpaces = this.GetIndexOfAll(text, " ");
+            for (let i = 1; i < indexOfWhiteSpaces.length; i++) {
+                const textBetweenSpaces = text.substring(
+                    indexOfWhiteSpaces[i - 1],
+                    indexOfWhiteSpaces[i]
+                );
+                if (textBetweenSpaces.length > 35) {
+                    let preivusPartOfText = text.substring(
+                        0,
+                        indexOfWhiteSpaces[i - 1]
+                    );
+                    let cutedText = text.substring(
+                        indexOfWhiteSpaces[i - 1],
+                        indexOfWhiteSpaces[i]
+                    );
+                    let resultText = cutedText.substring(0, 33);
+                    return preivusPartOfText + resultText + "...";
+                }
+            }
+            return text;
+        }
+        let cutedText = text.substring(0, length - 3);
+        let indexOfLastWhiteSpace = cutedText.lastIndexOf(" ");
+        // If there is no white space
+        if (indexOfLastWhiteSpace === -1)
+            indexOfLastWhiteSpace = cutedText.length - 3;
+        let cutedTextOnWhiteSpace = cutedText.substring(
+            0,
+            indexOfLastWhiteSpace
+        );
+        return cutedTextOnWhiteSpace + "...";
     }
     renderClassDate(daysOfWeek, classStartDate) {
         // daysOfWeek[2] = daysOfWeek[0];
@@ -53,7 +88,6 @@ class ClassTemplate extends Component {
             </ul>
         );
     }
-
     DaysOfWeekConvertor(dayNumber) {
         switch (dayNumber) {
             case 1:
@@ -74,6 +108,17 @@ class ClassTemplate extends Component {
                 console.log("Exeption Happened.");
                 break;
         }
+    }
+    GetIndexOfAll(text, character) {
+        let indexes = [0];
+
+        for (let i = 0; i < text.length; i++) {
+            const char = text[i];
+            if (char === character) indexes[indexes.length] = i;
+        }
+
+        indexes[indexes.length] = text.length;
+        return indexes;
     }
 }
 
