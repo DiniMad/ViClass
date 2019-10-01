e7f714b2b69771338ab34e10bb887115a545286a
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-01 12:53:53 +0330

    Create another API to get authenticated user classes.

--- ViClass/Controllers/ClassController.cs
+++ ViClass/Controllers/ClassController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using ViClass.Controllers.Resources;
 using ViClass.Data;
+using ViClass.Mapping;
 using ViClass.Models;
 
 namespace ViClass.Controllers
@@ -14,24 +17,48 @@ namespace ViClass.Controllers
     [Route("api/[controller]")]
     public class ClassController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext         _context;
+        private readonly IMapper                      _mapper;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ClassController(ApplicationDbContext context, IMapper mapper)
+        public ClassController(ApplicationDbContext context, IMapper mapper, UserManager<ApplicationUser> userManager)
         {
-            _context = context;
-            _mapper = mapper;
+            _context     = context;
+            _mapper      = mapper;
+            _userManager = userManager;
         }
 
         [HttpGet("[action]")]
-        public IEnumerable<ClassResource> Classes()
+        public async Task<IEnumerable<ClassResource>> Classes()
         {
-            var classes = _context.Classes
-                           .Include(c => c.Instructor)
-                           .Include(c => c.DayOfWeekSchedules)
-                           .Include(c => c.Videos)
-                           .ToList();
+            var classes = await _context.Classes
+                                        .Include(c => c.Instructor)
+                                        .Include(c => c.DayOfWeekSchedules)
+                                        .Include(c => c.Videos)
+                                        .ToListAsync();
             return _mapper.Map<List<Class>, List<ClassResource>>(classes);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<ClassResource>> StudentClasses()
+        {
+            var requestUser = await _userManager.GetUserAsync(HttpContext.User);
+
+            var user = await _context.Users
+                                     .Include(u => u.ClassesAsStudent)
+                                     .ThenInclude(cs => cs.Class)
+                                     .ThenInclude(c => c.Instructor)
+                                     .Include(u => u.ClassesAsStudent)
+                                     .ThenInclude(cs => cs.Class)
+                                     .ThenInclude(c => c.DayOfWeekSchedules)
+                                     .Include(u => u.ClassesAsStudent)
+                                     .ThenInclude(cs => cs.Class)
+                                     .ThenInclude(c => c.Videos)
+                                     .SingleAsync(u => u.Id == requestUser.Id);
+
+            var studentClasses = user.ClassesAsStudent.Select(cs => cs.Class).ToList();
+
+            return _mapper.Map<List<Class>, List<ClassResource>>(studentClasses);
         }
     }
 }
