f9e9104e5d2e63b295302f99be72072989ea9f53
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-04 20:20:19 +0330

    Add and use the slider component for showing the class items.

--- ViClass/ClientApp/src/components/Dashboard.jsx
+++ ViClass/ClientApp/src/components/Dashboard.jsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import { NavLink } from "reactstrap";
 import { Link } from "react-router-dom";
 import { ApplicationPaths } from "./api-authorization/ApiAuthorizationConstants";
-import ClassTemplate from "./ClassTemplate";
 import http from "./Services/HttpService";
 import config from "../config.json";
+import RecycleSlider from "./RecycleSlider";
+import ClassTemplate from "./ClassTemplate";
 
 const logoutPath = {
     pathname: `${ApplicationPaths.LogOut}`,
@@ -20,6 +21,10 @@ class Dashboard extends Component {
 
     componentDidMount = async () => {
         var { data: classes } = await http.get(studentClassesApi);
+        // TODO: remove the dummy items created below.
+        classes[1] = classes[2] = classes[3] = classes[4] = classes[5] = classes[6] = classes[7] =
+            classes[0];
+        console.log(classes);
         this.setState({ classes });
     };
 
@@ -29,17 +34,20 @@ class Dashboard extends Component {
                 <NavLink tag={Link} className="text-dark" to={logoutPath}>
                     Logout
                 </NavLink>
-                {this.state.classes &&
-                    this.state.classes.map(c => (
-                        <ClassTemplate
-                            key={c.id}
-                            title={c.title}
-                            description={c.description}
-                            instructor={c.instructor}
-                            dayOfWeekSchedules={c.dayOfWeekSchedules}
-                            startDateFormatted={c.startDateFormatted}
-                        ></ClassTemplate>
-                    ))}
+                {this.state.classes && (
+                    <RecycleSlider itemCountToShow={5} itemWidth={300}>
+                        {this.state.classes.map(c => (
+                            <ClassTemplate
+                                key={c.id}
+                                title={c.title}
+                                description={c.description}
+                                instructor={c.instructor}
+                                dayOfWeekSchedules={c.dayOfWeekSchedules}
+                                startDateFormatted={c.startDateFormatted}
+                            ></ClassTemplate>
+                        ))}
+                    </RecycleSlider>
+                )}
             </React.Fragment>
         );
     }
