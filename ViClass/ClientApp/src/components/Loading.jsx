824efe172f8fbd77b8689bc5429203f26712d59c
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-22 02:56:58 +0330

    Refactor Loading.jsx to a functional component and work with hooks.

--- ViClass/ClientApp/src/components/Loading.jsx
+++ ViClass/ClientApp/src/components/Loading.jsx
@@ -1,23 +1,25 @@
-import React, { Component } from "react";
+import React, {useEffect} from "react";
 import authService from "./api-authorization/AuthorizeService";
 import "../styles/loading.css";
 
-class Loading extends Component {
-    componentDidUpdate = async () => {
+function Loading(props) {
+    useEffect(() => {
+        // noinspection JSIgnoredPromiseFromCall
+        authenticateUser();
+    });
+    const authenticateUser = async () => {
         const url = window.location.href;
         const result = await authService.completeSignIn(url);
         if (result.status !== "success")
             console.log("Error: " + result.message);
     };
-    render() {
-        return (
-            <div id="loader-wrapper">
-                <div id="loader"></div>
-
-                <div className="loader-section section-left"></div>
-                <div className="loader-section section-right"></div>
-            </div>
-        );
-    }
+    return (
+        <div id="loader-wrapper">
+            <div id="loader"/>
+            <div className="loader-section section-left"/>
+            <div className="loader-section section-right"/>
+        </div>
+    );
 }
+
 export default Loading;
