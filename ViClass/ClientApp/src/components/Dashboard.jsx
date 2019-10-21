48ccf98f8123877ccd669f753d7a6d797839a113
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-22 03:14:44 +0330

    Refactor Dashboard.jsx to a functional component and work with hooks.

--- ViClass/ClientApp/src/components/Dashboard.jsx
+++ ViClass/ClientApp/src/components/Dashboard.jsx
@@ -1,51 +1,47 @@
-import React, { Component } from "react";
-
+import React, {useState, useEffect} from "react";
 import http from "./Services/HttpService";
 import config from "../config.json";
 import RecycleSlider from "./RecycleSlider";
 import ClassTemplate from "./ClassTemplate";
 import Navbar from "./Navbar";
 
-
-
 const studentClassesApi = config.ApiEndpoints.StudentClass;
 
-class Dashboard extends Component {
-    state = {
-        classes: null
-    };
+function Dashboard() {
+    const [classes, setClasses] = useState(null);
 
-    componentDidMount = async () => {
-        var { data: classes } = await http.get(studentClassesApi);
+    useEffect(() => {
+        // noinspection JSIgnoredPromiseFromCall
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const {data: classes} = await http.get(studentClassesApi);
         // TODO: remove the dummy items created below.
         classes[1] = classes[2] = classes[3] = classes[4] = classes[5] = classes[6] = classes[7] =
             classes[0];
-        console.log(classes);
-        this.setState({ classes });
+        setClasses(classes);
     };
-
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar/>
-   
-                {this.state.classes && (
-                    <RecycleSlider itemCountToShow={5} itemWidth={300}>
-                        {this.state.classes.map(c => (
-                            <ClassTemplate
-                                key={c.id}
-                                title={c.title}
-                                description={c.description}
-                                instructor={c.instructor}
-                                dayOfWeekSchedules={c.dayOfWeekSchedules}
-                                startDateFormatted={c.startDateFormatted}
-                            />
-                        ))}
-                    </RecycleSlider>
-                )}
-            </React.Fragment>
-        );
-    }
+    // noinspection JSUnresolvedVariable
+    return (
+        <React.Fragment>
+            <Navbar/>
+            {classes && (
+                <RecycleSlider itemCountToShow={5} itemWidth={300}>
+                    {classes.map(c => (
+                        <ClassTemplate
+                            key={c.id}
+                            title={c.title}
+                            description={c.description}
+                            instructor={c.instructor}
+                            dayOfWeekSchedules={c.dayOfWeekSchedules}
+                            startDateFormatted={c.startDateFormatted}
+                        />
+                    ))}
+                </RecycleSlider>
+            )}
+        </React.Fragment>
+    );
 }
 
 export default Dashboard;
