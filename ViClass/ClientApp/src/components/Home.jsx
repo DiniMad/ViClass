d47adbfaf2815a8d10074983ad6393d7bc0a29f7
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-22 02:46:07 +0330

    Refactor Home component to functional component and work with hooks.
    Also import statement in App component changed to point to Home component correctly.

--- ViClass/ClientApp/src/components/Home.jsx
+++ ViClass/ClientApp/src/components/Home.jsx
@@ -1,43 +1,45 @@
-import React, { Component } from "react";
-import { LoginRegisterOverlay } from "./LoginRegisterOverlay";
+import React, {useState, useEffect} from "react";
+import {LoginRegisterOverlay} from "./LoginRegisterOverlay";
 import login, {
     LoginResult
 } from "./api-authorization/AuthenticationAndApiAuthorization";
 
-export class Home extends Component {
-    static displayName = Home.name;
 
-    state = {
-        loginIframeSource: null,
-        overlayStyle: "overlay",
-        frameStyle: "frame"
-    };
-    componentDidMount = async () => {
-        var result = await login();
+function Home(props) {
+    const [loginIframeSource, setLoginIframeSource] = useState(null);
+    const [overlayStyle, setOverlayStyle] = useState("overlay");
+    const [frameStyle, setFrameStyle] = useState("frame");
+
+    useEffect(() => {
+        authenticateUser();
+    }, []);
+    
+    const authenticateUser = async () => {
+        const result = await login();
         switch (result.status) {
             case LoginResult.Success:
-                this.props.history.replace("/dashboard");
+                props.history.replace("/dashboard");
                 break;
             case LoginResult.Redirect:
-                this.setState({
-                    loginIframeSource: result.redirectUrl,
-                    overlayStyle: "overlay show",
-                    frameStyle: "frame show"
-                });
+                setLoginIframeSource(result.redirectUrl);
+                setOverlayStyle("overlay show");
+                setFrameStyle("frame show");
                 break;
             default:
                 console.log(result.message);
                 break;
         }
     };
-    handleIframeLoaded = async event => {
+
+    const handleIframeLoaded = async event => {
         if (event.target.name === "register-page") {
             const url = window.frames["register-page"].location.href;
             const urlArray = url.split("/");
             const urlLastIndex = urlArray[urlArray.length - 1];
             if (urlLastIndex !== "Register")
-                this.props.history.replace("/dashboard");
-        } else {
+                props.history.replace("/dashboard");
+        }
+        else {
             const urlPathname = window.frames["login-page"].location.pathname;
             const urlPathnameArray = urlPathname.split("/");
             const urlPathnameFirstIndex = urlPathnameArray[1];
@@ -51,23 +53,25 @@ export class Home extends Component {
             }
         }
     };
-    render() {
-        return (
-            <section id="showcase">
-                <div className={this.state.overlayStyle}>
-                    <div className={this.state.frameStyle}>
-                        <LoginRegisterOverlay
-                            onIframeLoaded={this.handleIframeLoaded}
-                            loginIframeSrc={
-                                window.location.search !== ""
-                                    ? "/authentication/login" +
-                                      window.location.search
-                                    : this.state.loginIframeSource
-                            }
-                        />
-                    </div>
+
+    return (
+        <section id="showcase">
+            <div className={overlayStyle}>
+                <div className={frameStyle}>
+                    <LoginRegisterOverlay
+                        onIframeLoaded={handleIframeLoaded}
+                        loginIframeSrc={
+                            window.location.search !== ""
+                            ? "/authentication/login" +
+                                window.location.search
+                            : loginIframeSource
+                        }
+                    />
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
 }
+
+export default Home;
+
