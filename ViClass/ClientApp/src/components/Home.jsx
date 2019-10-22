94af662447b767cafab1a50baeb836745850816f
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-22 05:11:14 +0330

    Refactor LoginRegisterOverlay.jsx to a functional component that use hooks.

--- ViClass/ClientApp/src/components/Home.jsx
+++ ViClass/ClientApp/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {LoginRegisterOverlay} from "./LoginRegisterOverlay";
+import LoginRegisterOverlay from "./LoginRegisterOverlay";
 import login, {
     LoginResult
 } from "./api-authorization/AuthenticationAndApiAuthorization";
--- ViClass/ClientApp/src/components/LoginRegisterOverlay.jsx
+++ ViClass/ClientApp/src/components/LoginRegisterOverlay.jsx
@@ -1,53 +1,58 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 
-export class LoginRegisterOverlay extends Component {
-    state = { slideStyle: "slide", loginIframeSource: null };
+function LoginRegisterOverlay(props) {
 
-    LoadRegisterPage = () => {
-        this.setState({ slideStyle: "slide turn" });
+    const [slideStyle, setSlideStyle] = useState("slide");
+
+    const LoadRegisterPage = () => {
+        setSlideStyle("slide turn");
+    };
+    const LoadLoginPage = () => {
+        setSlideStyle("slide");
     };
 
-    LoadLoginPage = () => {
-        this.setState({ slideStyle: "slide" });
-    };
-    render() {
-        return (
-            <div className="slide-container">
-                <div className={this.state.slideStyle}>
-                    <div className="slide-front">
-                        <iframe
-                            title="login-page"
-                            src={this.props.loginIframeSrc}
-                            name="login-page"
-                            onLoad={this.props.onIframeLoaded}
-                        ></iframe>
-                        <div className="slide-switch-button">
-                            <button
-                                className="button"
-                                onClick={this.LoadRegisterPage}
-                            >
-                                ساخت حساب کاربری
-                            </button>
-                        </div>
+    // noinspection HtmlUnknownTarget,JSUnresolvedVariable
+    return (
+        <div className="slide-container">
+            <div className={slideStyle}>
+                <div className="slide-front">
+                    <iframe
+                        title="login-page"
+                        src={props.loginIframeSrc}
+                        name="login-page"
+                        onLoad={props.onIframeLoaded}
+                    />
+                    <div className="slide-switch-button">
+                        <button
+                            className="button"
+                            onClick={LoadRegisterPage}
+                        >
+                            ساخت حساب کاربری
+                        </button>
                     </div>
-                    <div className="slide-back">
-                        <iframe
-                            title="register-page"
-                            src="/Identity/Account/Register"
-                            name="register-page"
-                            onLoad={this.props.onIframeLoaded}
-                        ></iframe>
-                        <div className="slide-switch-button">
-                            <button
-                                className="button"
-                                onClick={this.LoadLoginPage}
-                            >
-                                ورود به حساب کاربری
-                            </button>
-                        </div>
+                </div>
+                <div className="slide-back">
+                    <iframe
+                        title="register-page"
+                        src="/Identity/Account/Register"
+                        name="register-page"
+                        onLoad={props.onIframeLoaded}
+                    />
+                    <div className="slide-switch-button">
+                        <button
+                            className="button"
+                            onClick={LoadLoginPage}
+                        >
+                            ورود به حساب کاربری
+                        </button>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default LoginRegisterOverlay;
+
+    
+   
