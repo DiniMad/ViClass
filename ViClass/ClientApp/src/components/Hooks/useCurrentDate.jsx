0d09e2d0528c804c5ca065a8b12ae00df4dcf64a
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-28 07:49:45 +0330

    Create a custom hook to fetch date from API.

---
+++ ViClass/ClientApp/src/components/Hooks/useCurrentDate.jsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from "react";
+import Http from "../Services/HttpService";
+import Config from "../../config.json"
+
+const currentDateApi = Config.ApiEndpoints.CurrentDate;
+
+function useCurrentDate() {
+    const [currentDate, setCurrentDate] = useState(null);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const date = await Http.get(currentDateApi);
+        setCurrentDate(date);
+    };
+
+    return currentDate;
+}
+
+export default useCurrentDate;
