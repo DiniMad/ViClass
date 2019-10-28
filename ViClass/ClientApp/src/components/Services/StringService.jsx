438504baf3e94f7b3429a8aa94582ab099e25acc
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-28 08:58:26 +0330

    Add a method to StringService to specify a date is past or not.

--- ViClass/ClientApp/src/components/Services/StringService.jsx
+++ ViClass/ClientApp/src/components/Services/StringService.jsx
@@ -31,3 +31,26 @@ export const summarizeText = (text, length) => {
     let cutOfTextOnWhiteSpace = cutOfText.substring(0, indexOfLastWhiteSpace);
     return cutOfTextOnWhiteSpace + "...";
 };
+export const isPastAsPersianDate = (dateToCheck, currentDate) => {
+    let dateYear, dateMonth, dateDay, nowYear, nowMonth, nowDay;
+    try {
+        // Destructuring properties from dates
+        dateYear = parseInt(dateToCheck.split("/")[0]);
+        dateMonth = parseInt(dateToCheck.split("/")[1]);
+        dateDay = parseInt(dateToCheck.split("/")[2]);
+        nowYear = parseInt(currentDate.split("/")[0]);
+        nowMonth = parseInt(currentDate.split("/")[1]);
+        nowDay = parseInt(currentDate.split("/")[2]);
+    } catch {
+        throw "Parameter is not in a valid persian date format.";
+    }
+
+    // If years not same
+    if (dateYear !== nowYear) return nowYear > dateYear;
+    // If months not same
+    if (dateMonth !== nowMonth) return nowMonth > dateMonth;
+    // If days not same
+    if (dateDay !== nowDay) return nowDay > dateDay;
+    // If it is same year same month and same day(means its today) then its not past
+    return false;
+};
