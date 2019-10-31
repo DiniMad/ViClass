ea25f0731f63ed14c62738b0952c89dd49bbf2f4
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 12:53:38 +0330

    Create a new action in class controller to return a single class object.

--- ViClass/Controllers/ClassController.cs
+++ ViClass/Controllers/ClassController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -8,7 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using ViClass.Controllers.Resources;
 using ViClass.Data;
-using ViClass.Mapping;
 using ViClass.Models;
 
 namespace ViClass.Controllers
@@ -17,15 +17,13 @@ namespace ViClass.Controllers
     [Route("api/[controller]")]
     public class ClassController : Controller
     {
-        private readonly ApplicationDbContext         _context;
-        private readonly IMapper                      _mapper;
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper              _mapper;
 
-        public ClassController(ApplicationDbContext context, IMapper mapper, UserManager<ApplicationUser> userManager)
+        public ClassController(ApplicationDbContext context, IMapper mapper)
         {
-            _context     = context;
-            _mapper      = mapper;
-            _userManager = userManager;
+            _context = context;
+            _mapper  = mapper;
         }
 
         [HttpGet("[action]")]
@@ -42,7 +40,7 @@ namespace ViClass.Controllers
         [HttpGet("[action]")]
         public async Task<IEnumerable<ClassResource>> StudentClasses()
         {
-            var userId=HttpContext.User.Claims.First(c=>c.Type=="sub").Value;
+            var userId = HttpContext.User.Claims.First(c => c.Type == "sub").Value;
 
             var user = await _context.Users
                                      .Include(u => u.ClassesAsStudent)
@@ -59,6 +57,28 @@ namespace ViClass.Controllers
             var studentClasses = user.ClassesAsStudent.Select(cs => cs.Class).ToList();
 
             return _mapper.Map<List<Class>, List<ClassResource>>(studentClasses);
+        }
+
+        [HttpGet("{classId}")]
+        public async Task<ActionResult<ClassResource>> GetClass(int classId)
+        {
+            var userId = HttpContext.User.Claims.First(c => c.Type == "sub").Value;
+
+            var theClass = await _context.Classes
+                                         .Include(c => c.Instructor)
+                                         .Include(c => c.DayOfWeekSchedules)
+                                         .Include(c => c.Videos)
+                                         .Include(c => c.Students)
+                                         .SingleOrDefaultAsync(c => c.Id == classId);
+
+            if (theClass is null) return NotFound("The Class Not Found.");
+
+            if (theClass.InstructorId == userId)
+            {
+                _context.Entry(theClass).Collection(s => s.Students).Query().Include(cs => cs.Student).Load();
+            }
+
+            return _mapper.Map<Class, ClassResource>(theClass);
         }
     }
 }
