83652776392a8fb7b7441c6df0f27011b6681141
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 12:43:07 +0330

    Migration for renaming  LengthTimeFormatted to EndTimeFormatted.

---
+++ ViClass/Migrations/20191030054557_LengthTimeFormattedPropertyInDayOfWeekScheduleTableRenamedToEndTimeFormatted.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class LengthTimeFormattedPropertyInDayOfWeekScheduleTableRenamedToEndTimeFormatted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LengthTimeFormatted",
+                table: "DayOfWeekSchedule",
+                newName: "EndTimeFormatted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndTimeFormatted",
+                table: "DayOfWeekSchedule",
+                newName: "LengthTimeFormatted");
+        }
+    }
+}
