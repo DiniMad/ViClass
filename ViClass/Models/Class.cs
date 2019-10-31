3d27d9f030afcb2326766d8f3b5475b4369893d4
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 12:39:59 +0330

    Reformat Class model. (nothing important happened)

--- ViClass/Models/Class.cs
+++ ViClass/Models/Class.cs
@@ -8,28 +8,29 @@ namespace ViClass.Models
     {
         public Class()
         {
-            Videos = new Collection<Video>();
-            Students = new Collection<ClassStudent>();
-            SharedFiles = new Collection<SharedFile>();
+            Videos             = new Collection<Video>();
+            Students           = new Collection<ClassStudent>();
+            SharedFiles        = new Collection<SharedFile>();
             DayOfWeekSchedules = new Collection<DayOfWeekSchedule>();
         }
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string InstructorId { get; set; }
-        public ApplicationUser Instructor { get; set; }
-        public ICollection<ClassStudent> Students { get; set; }
-        public bool ShouldPresentVideo { get; set; }
-        public ICollection<Video> Videos { get; set; }
-        public ICollection<SharedFile> SharedFiles { get; set; }
-        public ICollection<DayOfWeekSchedule> DayOfWeekSchedules { get; set; }
-        public string StartDateFormatted { get; set; }
-        public string EndDateFormatted { get; set; }
-        public byte PeriodInEveryXWeeks { get; set; }
-        public byte MinStudentNumber { get; set; }
-        public byte? MaxStudentNumber { get; set; }
-        public bool IsItPrivate { get; set; }
-        public ushort PriceInHezarToman { get; set; }
-        public string LinkToLiveBroadcast { get; set; }
+
+        public int                            Id                  { get; set; }
+        public string                         Title               { get; set; }
+        public string                         Description         { get; set; }
+        public string                         InstructorId        { get; set; }
+        public ApplicationUser                Instructor          { get; set; }
+        public ICollection<ClassStudent>      Students            { get; set; }
+        public bool                           ShouldPresentVideo  { get; set; }
+        public ICollection<Video>             Videos              { get; set; }
+        public ICollection<SharedFile>        SharedFiles         { get; set; }
+        public ICollection<DayOfWeekSchedule> DayOfWeekSchedules  { get; set; }
+        public string                         StartDateFormatted  { get; set; }
+        public string                         EndDateFormatted    { get; set; }
+        public byte                           PeriodInEveryXWeeks { get; set; }
+        public byte                           MinStudentNumber    { get; set; }
+        public byte?                          MaxStudentNumber    { get; set; }
+        public bool                           IsItPrivate         { get; set; }
+        public ushort                         PriceInHezarToman   { get; set; }
+        public string                         LinkToLiveBroadcast { get; set; }
     }
 }
