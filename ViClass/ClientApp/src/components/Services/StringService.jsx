e8b8a46b0dc7e09a0b752342a56a2969da61c5f1
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 13:04:33 +0330

    Refactor summarize function in StringService to work more logical.

--- ViClass/ClientApp/src/components/Services/StringService.jsx
+++ ViClass/ClientApp/src/components/Services/StringService.jsx
@@ -9,25 +9,25 @@ const getAllPositionsOfCharacter = (text, character) => {
     indexes[indexes.length] = text.length;
     return indexes;
 };
-export const summarizeText = (text, length) => {
+export const summarizeText = (text, length, lineWrapLength = 0) => {
     // Check if every line has at least one white space (for Description field).
-    if (text.length < length) {
+    if (lineWrapLength !== 0) {
         let indexOfWhiteSpaces = getAllPositionsOfCharacter(text, " ");
         for (let i = 1; i < indexOfWhiteSpaces.length; i++) {
             const textBetweenSpaces = text.substring(indexOfWhiteSpaces[i - 1], indexOfWhiteSpaces[i]);
-            if (textBetweenSpaces.length > 35) {
+            if (textBetweenSpaces.length > lineWrapLength) {
                 let previousPartOfText = text.substring(0, indexOfWhiteSpaces[i - 1]);
                 let cutOfText = text.substring(indexOfWhiteSpaces[i - 1], indexOfWhiteSpaces[i]);
-                let resultText = cutOfText.substring(0, 33);
+                let resultText = cutOfText.substring(0, lineWrapLength - 3);
                 return previousPartOfText + resultText + "...";
             }
         }
-        return text;
     }
+    if (text.length <= length) return text;
     let cutOfText = text.substring(0, length - 3);
     let indexOfLastWhiteSpace = cutOfText.lastIndexOf(" ");
     // If there is no white space
-    if (indexOfLastWhiteSpace === -1) indexOfLastWhiteSpace = cutOfText.length - 3;
+    if (indexOfLastWhiteSpace === -1) indexOfLastWhiteSpace = text.length - 3;
     let cutOfTextOnWhiteSpace = cutOfText.substring(0, indexOfLastWhiteSpace);
     return cutOfTextOnWhiteSpace + "...";
 };
