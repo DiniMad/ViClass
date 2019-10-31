6f63f87e5ef24668ed16faa968e5c55a7729bf17
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 13:11:34 +0330

    Make Dashboard component more reliable vs null properties.

--- ViClass/ClientApp/src/components/Dashboard.jsx
+++ ViClass/ClientApp/src/components/Dashboard.jsx
@@ -17,6 +17,10 @@ function Dashboard() {
 
     const getData = async () => {
         const { data: classes } = await http.get(studentClassesApi);
+        if (classes && classes.length < 1) {
+            setClasses(null);
+            return;
+        }
         // TODO: remove the dummy items created below.
         classes[1] = classes[2] = classes[3] = classes[4] = classes[5] = classes[6] = classes[7] = classes[0];
         setClasses(classes);
@@ -27,11 +31,10 @@ function Dashboard() {
             <Navbar />
             {classes && (
                 <RecycleSlider itemCountToShow={5} itemWidth={300}>
-                    {classes.map(c => (
-                        <ClassTemplate key={c.id} classObject={c} />
-                    ))}
+                    {classes.map(c => c && <ClassTemplate key={c.id} classObject={c} />)}
                 </RecycleSlider>
             )}
+            {/* TODO: Render classes that user is teaching or study */}
         </React.Fragment>
     );
 }
