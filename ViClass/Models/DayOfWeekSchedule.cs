c6f3c2625dd522cd4936672f87487e5288d9f87c
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-10-31 12:41:07 +0330

    Rename the LengthTimeFormatted to EndTimeFormatted.

--- ViClass/Models/DayOfWeekSchedule.cs
+++ ViClass/Models/DayOfWeekSchedule.cs
@@ -6,7 +6,7 @@ namespace ViClass.Models
         public int Id { get; set; }
         public DayOfWeek DayOfWeek { get; set; }
         public string StartTimeFormatted { get; set; }
-        public string LengthTimeFormatted { get; set; }
+        public string EndTimeFormatted { get; set; }
         public int ClassId { get; set; }
         public Class Class { get; set; }
     }
