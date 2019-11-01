72c9eed081a7f3a0a5076f28ab566b3ea11050f1
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-01 08:49:28 +0330

    Comment out part of HttpService to  implement later.

--- ViClass/ClientApp/src/components/Services/HttpService.jsx
+++ ViClass/ClientApp/src/components/Services/HttpService.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-axios.interceptors.response.use(null, error => {
-    const expectedError =
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500;
-    if (!expectedError) {
-        console.log("Logging the HTTP error ", error);
-    }
-    return Promise.reject(error);
-});
+// axios.interceptors.response.use(null, error => {
+//     // const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+//     // if (!expectedError) {
+//     //     console.log("Logging the HTTP error ", error);
+//     //     error.response.status = 600;
+//     //     error.response.data = "Unexpected error occured.";
+//     // }
+//
+//     return Promise.reject(error);
+// });
 export default {
     get: axios.get,
     post: axios.post
