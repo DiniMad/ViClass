53d0fcfa7b42ba8bdea7270ff232f39dedb5faec
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-01 08:47:03 +0330

    Creating the usePostData hook for HTTP post calls.

---
+++ ViClass/ClientApp/src/components/Hooks/usePostData.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Http from "../Services/HttpService";
+
+function usePostData(url) {
+    const [{ data, status }, setDataAndStatus] = useState({ data: null, status: null });
+    const [postObject, setPostObject] = useState(null);
+
+    const postData = async () => {
+        if (!postObject) throw 'You should pass a valid object using "setPostObject" function to be post to the API.';
+        let responseData, responseStatus;
+        try {
+            const response = await Http.post(url, postObject);
+            responseData = response.data;
+            responseStatus = response.status;
+        } catch (e) {
+            responseData = e.response.data;
+            responseStatus = e.response.status;
+        }
+
+        setDataAndStatus({ data: responseData, status: responseStatus });
+    };
+
+    return { data, responseStatus: status, postData, setPostObject };
+}
+
+export default usePostData;
