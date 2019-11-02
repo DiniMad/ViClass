621892565f665f66b736311f1e22940af35e39ab
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-02 15:41:55 +0330

    Add NameAndFamily and ImagePath properties to Applicationuser model.

--- ViClass/Models/ApplicationUser.cs
+++ ViClass/Models/ApplicationUser.cs
@@ -9,10 +9,12 @@ namespace ViClass.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string NameAndFamily { get; set; }
         public string StudentNumber { get; set; }
         public bool StudentNumberConfirmed { get; set; }
         public ICollection<Class> ClassesAsInstructor { get; set; }
         public ICollection<ClassStudent> ClassesAsStudent { get; set; }
         public ICollection<Survey> SurveysCreated { get; set; }
+        public string ImagePath { get; set; }
     }
 }
