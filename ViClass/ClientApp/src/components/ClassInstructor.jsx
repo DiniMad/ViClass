a21d37afd89c8294173674852ad542721184f7fe
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-02 13:35:33 +0330

    Implementing ClassInstructor component.

--- ViClass/ClientApp/src/components/ClassInstructor.jsx
+++ ViClass/ClientApp/src/components/ClassInstructor.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import UserProfileImage from "../image/UserProfileImage.svg";
+import { Link } from "react-router-dom";
+import { summarizeText } from "./Services/StringService";
 
-function ClassInstructor({instructor}) {
+function ClassInstructor({ instructor }) {
     // Destructuring properties from instructor
-    const {userName} = instructor;
-    
+    const { id, userName, image } = instructor;
+
     return (
         <div className="class-instructor">
-            <p>{userName}</p>
+            <img
+                src={image || UserProfileImage}
+                alt={image ? `${userName} image profile.` : "Default user profile image."}
+            />
+            <Link to={`/user/${id}`}>
+                <h3>{summarizeText(userName, 20)}</h3>
+            </Link>
         </div>
     );
 }
