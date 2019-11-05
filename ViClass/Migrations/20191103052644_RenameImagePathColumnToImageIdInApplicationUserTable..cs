bca602d50cc398c1361edbcdaf8fb931ad9e87ee
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-05 08:29:57 +0330

    Migrations for Renaming ImagePath to ImageId and make it index.

---
+++ ViClass/Migrations/20191103052644_RenameImagePathColumnToImageIdInApplicationUserTable..cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class RenameImagePathColumnToImageIdInApplicationUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "AspNetUsers",
+                newName: "ImageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageId",
+                table: "AspNetUsers",
+                newName: "ImagePath");
+        }
+    }
+}
---
+++ ViClass/Migrations/20191103053349_MakeImageIdColumnOfApplicationUserTableIndex..cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class MakeImageIdColumnOfApplicationUserTableIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageId",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ImageId",
+                table: "AspNetUsers",
+                column: "ImageId",
+                unique: true,
+                filter: "[ImageId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ImageId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageId",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
