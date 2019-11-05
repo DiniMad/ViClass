5056140942b0885bd10ce7336ff3336255d5189b
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-05 08:32:04 +0330

    Update ApplicationUserResource based on need.

--- ViClass/Controllers/Resources/ApplicationUserResource.cs
+++ ViClass/Controllers/Resources/ApplicationUserResource.cs
@@ -3,12 +3,13 @@
     public class ApplicationUserResource
     {
         public string Id { get; set; }
+        public string NameAndFamily { get; set; }
         public string StudentNumber { get; set; }
         public bool StudentNumberConfirmed { get; set; }
-        public string UserName { get; set; }
         public string Email { get; set; }
         public bool EmailConfirmed { get; set; }
         public string PhoneNumber { get; set; }
         public bool PhoneNumberConfirmed { get; set; }
+        public string ImageId { get; set; }
     }
 }
