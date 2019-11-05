edf8eaf5c2280579343ccfdffd44f276fb3e4ef5
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-05 08:32:52 +0330

    A resource to return image from API.

---
+++ ViClass/Controllers/Resources/ImageResource.cs
@@ -0,0 +1,8 @@
+namespace ViClass.Controllers.Resources
+{
+    public class ImageResource
+    {
+        public string Data      { get; set; }
+        public string Extension { get; set; }
+    }
+}
