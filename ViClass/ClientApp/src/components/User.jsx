3851e5f378ff9c3b4b8434fc7841ebb48d9688db
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-05 09:21:18 +0330

    Update User component.

--- ViClass/ClientApp/src/components/User.jsx
+++ ViClass/ClientApp/src/components/User.jsx
@@ -1,20 +1,46 @@
-import React from "react";
-import Config from "../config";
-import useGetData from "./Hooks/useGetData";
+import React, {useContext} from "react";
 import Navbar from "./Navbar";
+import UserDetailBox from "./UserDetailBox";
+import UserImage from "./UserImage";
+import UserTextEditButton from "./UserTextEditButton";
+import {nameOrEmail} from "./Services/UserObjcetService";
+import {summarizeText} from "./Services/StringService";
+import useGetData from "./Hooks/useGetData";
+import UserContext from "./Context/UserContext";
+import MobileIcon from "../image/MobileIcon.svg"
+import StudentNumberIcon from "../image/StudentNumberIcon.svg"
+import EmailIcon from "../image/EmailIcon.svg"
+import Config from "../config";
+import UserDetails from "./UserDetails";
 
 const userApi = Config.ApiEndpoints.User;
 
 function User(props) {
     const userId = props.match.params.id;
 
-    const { data: user, responseStatus } = useGetData(userApi + userId);
+    const browserUser = useContext(UserContext);
+    const {data: user, responseStatus} = useGetData(userApi + userId);
+
+    const isHimSelf = browserUser && browserUser.sub === userId;
 
     return (
         <>
-            <Navbar />
+            <Navbar/>
             {responseStatus === 404 && <p>کاربر وجود ندارد.</p>}
-            {responseStatus === 200 && user && <div>{user.userName}</div>}
+            {responseStatus === 200 && user && (
+                <div className="container-percent">
+                    <div className="user">
+                        <UserImage imageId={user.imageId} username={nameOrEmail(user)}/>
+                        <div className='user-name'>
+                            <h1>{summarizeText(nameOrEmail(user), 30)}</h1>
+                            {isHimSelf && <UserTextEditButton name="username" text={nameOrEmail(user)}/>}
+                        </div>
+                        <UserDetails user={user} isHimSelf={isHimSelf}/>
+
+                        {isHimSelf && <div className="user-classes">Classes</div>}
+                    </div>
+                </div>
+            )}
         </>
     );
 }
