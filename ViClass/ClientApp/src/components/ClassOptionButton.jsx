87412832cfc0d6496e7399b75329372fc6279b99
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-05 09:02:04 +0330

    Render ClassOptionButton based on user relation with the class.

--- ViClass/ClientApp/src/components/ClassOptionButton.jsx
+++ ViClass/ClientApp/src/components/ClassOptionButton.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import RemoveIcon from "../image/RemoveIcon.svg";
+import PlusIcon from "../image/PlusIcon.svg";
 
-function ClassOptionButton({ classId, relationWithUser }) {
+function ClassOptionButton({classId, relationWithUser}) {
     const none = 0,
         student = 1,
         instructor = 2;
+
+    const className = relationWithUser === none
+                      ? "sub"
+                      : relationWithUser === student
+                        ? "unsub"
+                        : "remove";
+
+    const imageSource = relationWithUser === instructor
+                        ? RemoveIcon
+                        : PlusIcon;
+
+    const imageAltText = relationWithUser === none
+                         ? "Subscribe button"
+                         : relationWithUser === student
+                           ? "Unsubscribe button"
+                           : "Remove icon";
+
+    const title = relationWithUser === none
+                  ? "Subscribe"
+                  : relationWithUser === student
+                    ? "Unsubscribe"
+                    : "Remove";
 
     const handleOptionButton = async () => {
         // TODO: Implement functionality based on relationWithUser property
@@ -22,13 +45,8 @@ function ClassOptionButton({ classId, relationWithUser }) {
 
     return (
         <button className="class-option-button" onClick={handleOptionButton}>
-            <div
-                className={
-                    "class-option-button-content " +
-                    (relationWithUser === none ? "sub" : relationWithUser === student ? "unsub" : "remove")
-                }
-            >
-                {relationWithUser === instructor ? <img src={RemoveIcon} alt="Remove icon" /> : <p>+</p>}
+            <div className={"class-option-button-content " + className}>
+                <img title={title} src={imageSource} alt={imageAltText}/>
             </div>
         </button>
     );
