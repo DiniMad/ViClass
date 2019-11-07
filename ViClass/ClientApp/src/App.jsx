c5bf4d484359da18c6129f2408d9224471609093
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-07 16:05:52 +0330

    Add notification option to App.jsx using context and useNotification.

--- ViClass/ClientApp/src/App.jsx
+++ ViClass/ClientApp/src/App.jsx
@@ -11,32 +11,40 @@ import Loading from "./components/Loading";
 import CurrentDateContext from "./components/Context/CurrentDateContext";
 import Class from "./components/Class";
 import User from "./components/User";
+import Notification from "./components/Notification";
 import useGetData from "./components/Hooks/useGetData";
 import useAuthenticateUser from "./components/Hooks/useAuthenticateUser";
+import useNotification from "./components/Hooks/useNotification";
 import UserContext from "./components/Context/UserContext";
+import NotificationContext from "./components/Context/NotificationContext";
 import Config from "./config";
 import "./styles/style.min.css";
 
 const currentDateApi = Config.ApiEndpoints.CurrentDate;
 
 export default function App() {
+
     const {data: currentDate, responseStatus} = useGetData(currentDateApi);
     const user = useAuthenticateUser();
+    const {display, textTag, notificationType, displayNotification} = useNotification();
 
     return (
         <React.Fragment>
-            <Route exact path="/" component={Home}/>
-            <Route path="/counter" component={Counter}/>
-            <Route path="/loading" component={Loading}/>
-            <AuthorizeRoute path="/fetch-data" component={FetchData}/>
-            <UserContext.Provider value={user}>
-                <CurrentDateContext.Provider value={responseStatus === 200 && currentDate}>
-                    <AuthorizeRoute path="/dashboard" component={Dashboard}/>
-                    <AuthorizeRoute path="/class/:id" component={Class}/>
-                    <AuthorizeRoute path="/user/:id" component={User}/>
-                </CurrentDateContext.Provider>
-            </UserContext.Provider>
-            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
+            <NotificationContext.Provider value={displayNotification}>
+                <Route exact path="/" component={Home}/>
+                <Route path="/counter" component={Counter}/>
+                <Route path="/loading" component={Loading}/>
+                <AuthorizeRoute path="/fetch-data" component={FetchData}/>
+                <UserContext.Provider value={user}>
+                    <CurrentDateContext.Provider value={responseStatus === 200 && currentDate}>
+                        <AuthorizeRoute path="/dashboard" component={Dashboard}/>
+                        <AuthorizeRoute path="/class/:id" component={Class}/>
+                        <AuthorizeRoute path="/user/:id" component={User}/>
+                    </CurrentDateContext.Provider>
+                </UserContext.Provider>
+                <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
+                <Notification display={display} textTag={textTag} type={notificationType}/>
+            </NotificationContext.Provider>
         </React.Fragment>
     );
 }
