63c92fe98ed190b44e2f93173ef60cd5c63e9e73
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-07 14:32:17 +0330

    Add two new function.
    A function to set a file to image and a function to crop image and returns it as a file.

--- ViClass/ClientApp/src/components/Services/ImageService.jsx
+++ ViClass/ClientApp/src/components/Services/ImageService.jsx
@@ -1,2 +1,37 @@
+import {dataURLtoFile} from "./FileService";
+
 // A function to convert fetched image object to actual image
 export const getImageFromBase64 = (image) => `data:image/${image.extension}jpg;base64,${image.data}`;
+
+export const setImageFromFile = (image, setImage) => {
+
+    const fileReader = new FileReader();
+    fileReader.onload = function (e) {
+        setImage(e.target.result);
+    };
+
+    fileReader.readAsDataURL(image);
+};
+
+export const getCroppedImageAsFile = async (image, crop, fileName) => {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const canvasContext = canvas.getContext('2d');
+
+    canvasContext.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width,
+        crop.height
+    );
+    const canvasImage = await canvas.toDataURL('image/jpeg');
+    return dataURLtoFile(canvasImage, fileName);
+};
