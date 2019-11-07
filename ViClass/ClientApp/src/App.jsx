98ecd84f7dd84c7bd1b1acba63e9e701a07ca042
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-07 14:28:42 +0330

    Remove commented Layout component and its import statement.

--- ViClass/ClientApp/src/App.jsx
+++ ViClass/ClientApp/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Route } from "react-router";
-import { Layout } from "./components/Layout";
+import {Route} from "react-router";
 import Home from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
+import {FetchData} from "./components/FetchData";
+import {Counter} from "./components/Counter";
 import Dashboard from "./components/Dashboard";
 import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
-import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
+import {ApplicationPaths} from "./components/api-authorization/ApiAuthorizationConstants";
 import Loading from "./components/Loading";
 import CurrentDateContext from "./components/Context/CurrentDateContext";
 import Class from "./components/Class";
@@ -21,25 +20,23 @@ import "./styles/style.min.css";
 const currentDateApi = Config.ApiEndpoints.CurrentDate;
 
 export default function App() {
-    const { data: currentDate, responseStatus } = useGetData(currentDateApi);
+    const {data: currentDate, responseStatus} = useGetData(currentDateApi);
     const user = useAuthenticateUser();
 
     return (
         <React.Fragment>
-            {/* <Layout> */}
-            <Route exact path="/" component={Home} />
-            <Route path="/counter" component={Counter} />
-            <Route path="/loading" component={Loading} />
-            <AuthorizeRoute path="/fetch-data" component={FetchData} />
+            <Route exact path="/" component={Home}/>
+            <Route path="/counter" component={Counter}/>
+            <Route path="/loading" component={Loading}/>
+            <AuthorizeRoute path="/fetch-data" component={FetchData}/>
             <UserContext.Provider value={user}>
                 <CurrentDateContext.Provider value={responseStatus === 200 && currentDate}>
-                    <AuthorizeRoute path="/dashboard" component={Dashboard} />
-                    <AuthorizeRoute path="/class/:id" component={Class} />
-                    <AuthorizeRoute path="/user/:id" component={User} />
+                    <AuthorizeRoute path="/dashboard" component={Dashboard}/>
+                    <AuthorizeRoute path="/class/:id" component={Class}/>
+                    <AuthorizeRoute path="/user/:id" component={User}/>
                 </CurrentDateContext.Provider>
             </UserContext.Provider>
-            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-            {/* </Layout> */}
+            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
         </React.Fragment>
     );
 }
