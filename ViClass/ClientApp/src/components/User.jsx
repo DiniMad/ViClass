5fe5dca8e54a81f7f5ce5e28e7742582b9b4d752
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-11 23:51:19 +0330

    Refactoring User component.

--- ViClass/ClientApp/src/components/User.jsx
+++ ViClass/ClientApp/src/components/User.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import Navbar from "./Navbar";
 import UserImage from "./UserImage";
 import UserTextEditButton from "./UserTextEditButton";
@@ -6,16 +6,25 @@ import UserDetails from "./UserDetails";
 import {nameOrEmail} from "./Services/UserObjcetService";
 import {summarizeText} from "./Services/StringService";
 import useGetData from "./Hooks/useGetData";
-import UserContext from "./Context/UserContext";
+import AuthenticatedUserContext from "./Context/AuthenticatedUserContext";
 import Config from "../config";
+import UserContext from "./Context/UserContext";
+import UserNameAndFamily from "./UserNameAndFamily";
 
 const userApi = Config.ApiEndpoints.User;
 
 function User(props) {
     const userId = props.match.params.id;
 
-    const browserUser = useContext(UserContext);
-    const {data: user, responseStatus} = useGetData(userApi + userId);
+    const [user, setUser] = useState(null);
+
+    const browserUser = useContext(AuthenticatedUserContext);
+    const {data, responseStatus} = useGetData(userApi + userId);
+
+    useEffect(() => {
+        if (responseStatus === 200 && data)
+            setUser(data);
+    }, [data, responseStatus]);
 
     const isHimSelf = browserUser && browserUser.sub === userId;
 
@@ -23,19 +32,17 @@ function User(props) {
         <>
             <Navbar/>
             {responseStatus === 404 && <p>کاربر وجود ندارد.</p>}
-            {responseStatus === 200 && user && (
-                <div className="container-percent">
-                    <div className="user">
-                        <UserImage imageId={user.imageId} username={nameOrEmail(user)}/>
-                        <div className='user-name'>
-                            <h1>{summarizeText(nameOrEmail(user), 30)}</h1>
-                            {isHimSelf && <UserTextEditButton name="username" text={nameOrEmail(user)}/>}
+            {user && (
+                <UserContext.Provider value={{user, setUser}}>
+                    <div className="container-percent">
+                        <div className="user">
+                            <UserImage imageId={user.imageId} username={nameOrEmail(user)}/>
+                            <UserNameAndFamily user={user} isHimSelf={isHimSelf}/>
+                            <UserDetails user={user} isHimSelf={isHimSelf}/>
+                            {isHimSelf && <div className="user-classes">Classes</div>}
                         </div>
-                        <UserDetails user={user} isHimSelf={isHimSelf}/>
-
-                        {isHimSelf && <div className="user-classes">Classes</div>}
                     </div>
-                </div>
+                </UserContext.Provider>
             )}
         </>
     );
