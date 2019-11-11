e240f906bcbee16e1625f55c62dc237932ffe3ec
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-11 23:31:42 +0330

    A new action in "UserController" to update user details.

--- ViClass/Controllers/UserController.cs
+++ ViClass/Controllers/UserController.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ViClass.Controllers.Resources;
 using ViClass.Data;
 using ViClass.Models;
+using ViClass.Utility;
 
 namespace ViClass.Controllers
 {
+    [Authorize(AuthenticationSchemes = "Identity.Application")]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
@@ -37,6 +43,34 @@ namespace ViClass.Controllers
             }
 
             return _mapper.Map<ApplicationUser, ApplicationUserResource>(user);
+        }
+
+        // PUT: api/user/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClass(string id, ApplicationUserResource inputUser)
+        {
+            var requestUserId = HttpContext.User.Claims.First(c => c.Type == "sub").Value;
+
+            if (id != requestUserId) return BadRequest();
+
+            var storageUser = await _context.Users.FindAsync(id);
+
+            var validationError = storageUser.ModifyChangeIfItsValid(inputUser);
+            if (!string.IsNullOrWhiteSpace(validationError)) return BadRequest(validationError);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is SqlException innerException &&
+                    (innerException.Number == 2627 || innerException.Number == 2601))
+                    return StatusCode(403, "Already taken.");
+                return StatusCode(500, "Something went wrong.");
+            }
+
+            return Ok(Guid.NewGuid());
         }
     }
 }
