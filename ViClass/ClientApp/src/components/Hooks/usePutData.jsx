bad360d9f1b7bea47751f3191ccb0a87d671c1d8
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-11 23:40:48 +0330

    A custom hook for HTTP put method.

---
+++ ViClass/ClientApp/src/components/Hooks/usePutData.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+import Http from "../Services/HttpService";
+
+function usePutData(url) {
+    const [{data, status}, setDataAndStatus] = useState({data: null, status: null});
+
+    const put = async (object) => {
+        if (!object) throw 'You should pass a valid object using "setPostObject" function to be post to the API.';
+        let responseData, responseStatus;
+        try {
+            const response = await Http.put(url, object);
+            responseData = response.data;
+            responseStatus = response.status;
+        } catch (e) {
+            responseData = e.response.data;
+            responseStatus = e.response.status;
+        }
+
+        setDataAndStatus({data: responseData, status: responseStatus});
+    };
+
+    return {data, responseStatus: status, put};
+}
+
+export default usePutData;
