0cdddc3440597704d860d644e8cf3cdbac6d561e
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-13 18:07:42 +0330

    Refactor DayOfWeek to use new DateService functions.

--- ViClass/ClientApp/src/components/DayOfWeek.jsx
+++ ViClass/ClientApp/src/components/DayOfWeek.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {daysOfWeekConverter} from "./Services/DateService";
+import DateService from "./Services/DateService";
 
 function DayOfWeek({dayOfWeekSchedules}) {
-    
+    const {daysOfWeekConverter} = DateService();
     // Destructuring properties from dayOfWeekSchedules
     const {dayOfWeek, startTimeFormatted, endTimeFormatted} = dayOfWeekSchedules;
-    
+
     return (
         <div className="class-date-days-template">
             <p>{daysOfWeekConverter(dayOfWeek)}</p>
