3ee8c2dd9bd6c577ad69fdea7fd12061641361c4
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-13 18:09:26 +0330

    A new component to display limited details of classes as a table row.

---
+++ ViClass/ClientApp/src/components/ClassTableViewItem.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {nameOrEmail} from "./Services/UserObjcetService";
+import {summarizeText} from "./Services/StringService";
+import DateService from "./Services/DateService";
+import InstructorIcon from "../image/InstructorIcon.svg"
+import StudentIcon from "../image/StudentIcon.svg"
+
+function ClassTableViewItem({classObject}) {
+    let dateBadgeClasses, dateBadgeText;
+    // Destructuring properties
+    let {
+        id,
+        title,
+        description,
+        instructor,
+        dayOfWeekSchedules,
+        startDateFormatted,
+        endDateFormatted,
+        relationWithUser,
+        students
+    } = classObject;
+    dayOfWeekSchedules[0].dayOfWeek = 4;
+    dayOfWeekSchedules[1].dayOfWeek = 4;
+    const {whenIsNextClassDate} = DateService();
+    [dateBadgeClasses, dateBadgeText] = whenIsNextClassDate(startDateFormatted, endDateFormatted, dayOfWeekSchedules);
+
+    return (
+        <div className="class-table-view-item">
+            <div className="class-table-view-item-icon">
+                <img src={relationWithUser === 2
+                          ? InstructorIcon
+                          : StudentIcon} alt="Instructor icon."/>
+            </div>
+            <div className="class-table-view-item-title">
+                <Link to={`/class/${id}`}><p>{summarizeText(title, 35)}</p></Link>
+            </div>
+            <div className="class-table-view-item-description">
+                <p>{summarizeText(description, 55)}</p>
+            </div>
+            <div className="class-table-view-item-instructor">
+                مدرس: <Link to={`/user/${instructor.id}`}>{summarizeText(nameOrEmail(instructor), 22)}</Link>
+            </div>
+            <div className="class-table-view-item-date">
+                <p className={dateBadgeClasses}>{dateBadgeText}</p>
+            </div>
+            <div className="class-table-view-item-students-count">{`${students.length} دانشجو`}</div>
+        </div>
+    );
+}
+
+export default ClassTableViewItem;
