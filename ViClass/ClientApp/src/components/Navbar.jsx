78e9ab2190d0f4db090cc4ae193e5e74e93ac96e
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-13 18:08:30 +0330

    Add link to dashboard to Navbar component.

--- ViClass/ClientApp/src/components/Navbar.jsx
+++ ViClass/ClientApp/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
-import { NavLink } from "reactstrap";
-import { Link } from "react-router-dom";
-import { ApplicationPaths } from "./api-authorization/ApiAuthorizationConstants";
+import React, {useContext} from "react";
+import {NavLink} from "reactstrap";
+import {Link} from "react-router-dom";
+import {ApplicationPaths} from "./api-authorization/ApiAuthorizationConstants";
 import AuthenticatedUserContext from "./Context/AuthenticatedUserContext";
 
 const logoutPath = {
     pathname: `${ApplicationPaths.LogOut}`,
-    state: { local: true }
+    state: {local: true}
 };
 
 function Navbar() {
@@ -17,12 +17,9 @@ function Navbar() {
             {user && (
                 <>
                     {/* TODO: It is better to change the link parameter to username instead of userid*/}
-                    <Link to={`/user/${user.sub}`}>
-                        <p>{user && user.name}</p>
-                    </Link>
-                    <NavLink tag={Link} className="text-dark" to={logoutPath}>
-                        Logout
-                    </NavLink>
+                    <Link to={`/user/${user.sub}`}><p>{user && user.name}</p></Link>
+                    <Link to="/dashboard">داشبورد</Link>
+                    <NavLink tag={Link} className="text-dark" to={logoutPath}>خروج</NavLink>
                 </>
             )}
         </nav>
