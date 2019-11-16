3b506dff3f24bceb111185b36c3328d26b75e19d
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-16 16:06:58 +0330

    Refactor component to make sure date service initiated.

--- ViClass/ClientApp/src/components/DayOfWeek.jsx
+++ ViClass/ClientApp/src/components/DayOfWeek.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import DateService from "./Services/DateService";
 
 function DayOfWeek({dayOfWeekSchedules}) {
-    const {daysOfWeekConverter} = DateService();
+    const {dateServiceInitiated, daysOfWeekConverter} = DateService();
     // Destructuring properties from dayOfWeekSchedules
     const {dayOfWeek, startTimeFormatted, endTimeFormatted} = dayOfWeekSchedules;
 
     return (
         <div className="class-date-days-template">
-            <p>{daysOfWeekConverter(dayOfWeek)}</p>
+            <p>{dateServiceInitiated && daysOfWeekConverter(dayOfWeek)}</p>
             <p>{startTimeFormatted}</p>
             <p>{endTimeFormatted}</p>
         </div>
