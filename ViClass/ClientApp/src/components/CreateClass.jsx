f8ecacb6a5858e587f4b0efb5efcea21843bd912
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-18 21:23:56 +0330

    Add validation on submit to CreateClass component.

--- ViClass/ClientApp/src/components/CreateClass.jsx
+++ ViClass/ClientApp/src/components/CreateClass.jsx
@@ -1,30 +1,96 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, useContext} from 'react';
 import InputText from "./InputText";
 import Navbar from "./Navbar";
 import InputDayOfWeekSchedule from "./InputDayOfWeekSchedule";
 import DateDetails from "./DateDetails";
 import StudentNumberDetails from "./StudentNumberDetails";
+import NotificationContext from "./Context/NotificationContext";
+import ClassService from "./Services/ClassService";
+import usePostData from "./Hooks/usePostData";
+import Config from "../config";
+
+const classApi = Config.ApiEndpoints.Class;
+const titleMaxLengthAllowed = Config.ModelMaxLengthAllowed.ClassTitle;
+const descriptionMaxLengthAllowed = Config.ModelMaxLengthAllowed.ClassDescription;
 
 function CreateClass() {
+    const {validateStudentsNumber, validationDateDetails, validateDayOfWeekSchedule} = ClassService();
 
-    const [dayOfWeekSchedule, setDayOfWeekSchedule] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [dayOfWeekSchedules, setDayOfWeekSchedule] = useState([]);
     const [startDateFormatted, setStartDateFormatted] = useState("");
     const [endDateFormatted, setEndDateFormatted] = useState("");
-    const [minStudentsNumber, setMinStudentsNumber] = useState("");
-    const [maxStudentsNumber, setMaxStudentsNumber] = useState("");
+    const [minStudentNumber, setMinStudentsNumber] = useState("");
+    const [maxStudentNumber, setMaxStudentsNumber] = useState("");
 
     const warningElement = useRef(null);
 
+    const displayNotification = useContext(NotificationContext);
+
+    const [creatClassResponseData, creatClassResponseStatus, postNewClass] = usePostData(classApi);
+
     useEffect(() => {
         if (!warningElement) return;
-        if (dayOfWeekSchedule.length > 0) warningElement.current.classList.add("display");
+        if (dayOfWeekSchedules.length > 0) warningElement.current.classList.add("display");
         else warningElement.current.classList.remove("display");
-    }, [dayOfWeekSchedule.length]); // Display warning message if any day selected
+    }, [dayOfWeekSchedules.length]); // Display warning message if any day selected
+    useEffect(() => {
+        if (!creatClassResponseData) return;
 
-    const handleSubmit = e => {
+        if (creatClassResponseStatus === 201) displayNotification("کلاس با موفقیت ایجاد شده.", 5, "success");
+        else displayNotification("مشکلی در حین ایجاد کلاس بوجود آمده است.", 5, "warning");
+
+        setTitle("");
+        setDescription("");
+        setDayOfWeekSchedule([]);
+        setStartDateFormatted("");
+        setEndDateFormatted("");
+        setMinStudentsNumber("");
+        setMaxStudentsNumber("");
+    }, [creatClassResponseData, creatClassResponseStatus]);
+
+
+    const handleSubmit = async e => {
+        let error;
         e.preventDefault();
+
         // Validation object properties 
+        if (!title.trim()) {                                                        // Validate Tittle
+            displayNotification("عنوان نمیتواند خالی باشد.", 5, "warning");
+            return;
+        }
+        if (!description.trim()) {                                                  // Validate Description
+            displayNotification("توضیحات نمیتواند خالی باشد.", 5, "warning");
+            return;
+        }
+        error = validateStudentsNumber(minStudentNumber, maxStudentNumber);       // Validate Students Number
+        if (error) {
+            displayNotification(error, 5, "warning");
+            return;
+        }
+        error = await validationDateDetails(startDateFormatted, endDateFormatted);  // Validate Date Details
+        if (error) {
+            displayNotification(error, 5, "warning");
+            return;
+        }
+        error = validateDayOfWeekSchedule(dayOfWeekSchedules);                       // Validate Schedule
+        if (error) {
+            displayNotification(error, 5, "warning");
+            return;
+        }
+
         // Post object
+        const newClass = {
+            title,
+            description,
+            dayOfWeekSchedules,
+            startDateFormatted,
+            endDateFormatted,
+            minStudentNumber,
+            maxStudentNumber
+        };
+        postNewClass(newClass);
     };
 
     return (
@@ -32,8 +98,11 @@ function CreateClass() {
             <Navbar/>
             <div className="create-class">
                 <form onSubmit={handleSubmit}>
-                    <InputText label="عنوان" name="title" length={30} inputRegex={undefined}/>
-                    <InputText label="توضیحات" name="description" length={250} inputRegex={undefined} lines={4}/>
+                    <InputText label="عنوان" name="title" length={titleMaxLengthAllowed} inputRegex={undefined}
+                               setText={setTitle}/>
+                    <InputText label="توضیحات" name="description" length={descriptionMaxLengthAllowed}
+                               inputRegex={undefined} lines={6}
+                               setText={setDescription}/>
                     <StudentNumberDetails setMinimum={setMinStudentsNumber} setMaximum={setMaxStudentsNumber}/>
                     <DateDetails setStartDate={setStartDateFormatted} setEndDate={setEndDateFormatted}/>
                     <InputDayOfWeekSchedule setDayOfWeekSchedule={setDayOfWeekSchedule}/>
