b6bc2a11ef226ac22eb9776b365c91466feea498
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-18 20:41:27 +0330

    Change properties length migrations.

---
+++ ViClass/Migrations/20191118170533_SetTitleLengthTo64AndDescriptionLength512InApplicationUserTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class SetTitleLengthTo64AndDescriptionLength512InApplicationUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Classes",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Classes",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Classes",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Classes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 512);
+        }
+    }
+}
