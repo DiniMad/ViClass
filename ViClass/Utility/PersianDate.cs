af1d4efd48cad9d7eaef1a4818098b38cd14f915
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-18 21:09:47 +0330

    Add two new methods to PersianDate utility class.

--- ViClass/Utility/PersianDate.cs
+++ ViClass/Utility/PersianDate.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace ViClass.Utility
 {
-    public class PersianDate
+    public static class PersianDate
     {
         public static string GetPersianDateFormatted(bool withDayOfWeekNumber = false)
         {
@@ -15,6 +15,41 @@ namespace ViClass.Utility
             var dayOfWeek       = persianCalender.GetDayOfWeek(DateTime.Now);
             var dayOfWeekNumber = dayOfWeek.GetDayNumberInWeekStartsOnSaturday();
             return $"{year}/{month}/{day},{dayOfWeekNumber}";
+        }
+
+        public static bool IsDateGreaterThan(this string firstDate, string secondDate)
+        {
+            int firstYear, firstMonth, firstDay, secondYear, secondMonth, secondDay;
+
+            try
+            {
+                firstYear   = int.Parse(firstDate.Split('/')[0]);
+                firstMonth  = int.Parse(firstDate.Split('/')[1]);
+                firstDay    = int.Parse(firstDate.Split('/')[2]);
+                secondYear  = int.Parse(secondDate.Split('/')[0]);
+                secondMonth = int.Parse(secondDate.Split('/')[1]);
+                secondDay   = int.Parse(secondDate.Split('/')[2]);
+            }
+            catch
+            {
+                throw new ArgumentException("The parameter is not a valid persian date.");
+            }
+
+            if (firstYear  != secondYear) return firstYear   > secondYear;
+            if (firstMonth != secondMonth) return firstMonth > secondMonth;
+            if (firstDay   != secondDay) return firstDay     > secondDay;
+            // Same year, month and day so two dates are exactly same and first date is not greater than second
+            return false;
+        }
+
+        public static string GetTomorrow()
+        {
+            var persianCalender = new PersianCalendar();
+            var tomorrow        = DateTime.Now.AddDays(1);
+            var year            = persianCalender.GetYear(tomorrow);
+            var month           = persianCalender.GetMonth(tomorrow);
+            var day             = persianCalender.GetDayOfMonth(tomorrow);
+            return $"{year}/{month}/{day}";
         }
     }
 }
