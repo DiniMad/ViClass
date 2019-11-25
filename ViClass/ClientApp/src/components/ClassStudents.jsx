24acf178442b6bde831ffe963edca51e51c7557f
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-11-25 19:35:04 +0330

    Display maximum and minimum students number on hover.

--- ViClass/ClientApp/src/components/ClassStudents.jsx
+++ ViClass/ClientApp/src/components/ClassStudents.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useRef} from "react";
+import {Link} from "react-router-dom";
 import {nameOrEmail} from "./Services/UserObjcetService";
 
-function ClassStudents({ students }) {
+function ClassStudents({students, minNumber, maxNumber}) {
+
+    const studentNumberDetails = useRef(null);
+
+    const handleMouseEnter = () => studentNumberDetails.current.classList.add("hover");
+    const handleMouseLeave = () => studentNumberDetails.current.classList.remove("hover");
+
     return (
-        <div className="class-students">
+        <div className="class-students" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <h3 className="class-students-title">{students.length} دانشجو</h3>
+            <div ref={studentNumberDetails} className="class-students-number">
+                <div className="class-students-number-max"><p>{maxNumber || "∞"}</p></div>
+                <div className="class-students-number-min"><p>{minNumber}</p></div>
+            </div>
             <div className="class-students-items">
                 {students.map(
                     s =>
