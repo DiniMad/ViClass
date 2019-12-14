9ae43ef2271610db64531b8be963a3f381866ea5
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-14 19:41:17 +0330

    Cleanup the App.jsx component.

--- ViClass/ClientApp/src/App.jsx
+++ ViClass/ClientApp/src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import {Route} from "react-router";
-import Home from "./components/Home";
-import {FetchData} from "./components/FetchData";
-import {Counter} from "./components/Counter";
 import Dashboard from "./components/Dashboard";
 import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
 import {ApplicationPaths} from "./components/api-authorization/ApiAuthorizationConstants";
-import Loading from "./components/Loading";
 import CurrentDateContext from "./components/Context/CurrentDateContext";
 import Class from "./components/Class";
 import User from "./components/User";
@@ -17,10 +13,10 @@ import LiveClass from "./components/LiveClass";
 import useAuthenticateUser from "./components/Hooks/useAuthenticateUser";
 import useNotification from "./components/Hooks/useNotification";
 import useCurrentDate from "./components/Hooks/useCurrentDate";
+import Index from "./components/Index";
 import AuthenticatedUserContext from "./components/Context/AuthenticatedUserContext";
 import NotificationContext from "./components/Context/NotificationContext";
 import "./styles/style.min.css";
-import Index from "./components/Index";
 
 
 export default function App() {
@@ -33,9 +29,6 @@ export default function App() {
         <React.Fragment>
             <NotificationContext.Provider value={displayNotification}>
                 <Route exact path="/" component={Index}/>
-                <Route path="/counter" component={Counter}/>
-                <Route path="/loading" component={Loading}/>
-                <AuthorizeRoute path="/fetch-data" component={FetchData}/>
                 <AuthenticatedUserContext.Provider value={user}>
                     <CurrentDateContext.Provider value={currentDate && [currentDate, setCurrentDate]}>
                         <AuthorizeRoute path="/dashboard" component={Dashboard}/>
