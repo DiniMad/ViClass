c0ae1dd683969e9279fee023686e7a40eb8f1bc2
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-17 18:29:59 +0330

    Migration of the LinkToLiveBroadcast name changes to StreamKey.

---
+++ ViClass/Migrations/20191217145750_RenameLinkToLiveBroadcastColumnInClassTableToStreamKey.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class RenameLinkToLiveBroadcastColumnInClassTableToStreamKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LinkToLiveBroadcast",
+                table: "Classes",
+                newName: "StreamKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StreamKey",
+                table: "Classes",
+                newName: "LinkToLiveBroadcast");
+        }
+    }
+}
