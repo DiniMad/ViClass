560b1f9ff67b2541fa46d066a40db1bb2396a580
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-23 05:26:52 +0330

    Migrations for changing type of VolumeInMg property in SharedFile model.

---
+++ ViClass/Migrations/20191223012803_ChangeVolumeInMgColumnTypeToDoubleInSharedFileTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class ChangeVolumeInMgColumnTypeToDoubleInSharedFileTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VolumeInMg",
+                table: "SharedFile",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VolumeInMg",
+                table: "SharedFile",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
---
+++ ViClass/Migrations/20191223014258_ChangeVolumeInMgColumnTypeToStringInSharedFileTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class ChangeVolumeInMgColumnTypeToStringInSharedFileTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VolumeInMg",
+                table: "SharedFile",
+                nullable: true,
+                oldClrType: typeof(double));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VolumeInMg",
+                table: "SharedFile",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
