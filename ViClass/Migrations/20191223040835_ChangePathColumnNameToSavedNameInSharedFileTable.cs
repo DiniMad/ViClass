984fa16c1fc96f572f000f9b44dbbb97d42c203b
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-24 03:56:33 +0330

    Migration files for changing Name of Path in SharedFile model.

---
+++ ViClass/Migrations/20191223040835_ChangePathColumnNameToSavedNameInSharedFileTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class ChangePathColumnNameToSavedNameInSharedFileTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "SharedFile",
+                newName: "SavedName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SavedName",
+                table: "SharedFile",
+                newName: "Path");
+        }
+    }
+}
