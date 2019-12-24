ed18f41cc6bb09bcdf5fc912a8e95809644b66a1
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-24 03:55:25 +0330

    Rename name of property Path in SharedFile model to SavedName.
    Also its references like its resource and configuration file are changed.

--- ViClass/Controllers/Resources/SharedFileResource.cs
+++ ViClass/Controllers/Resources/SharedFileResource.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public string Path { get; set; }
+        public string SavedName { get; set; }
         public string VolumeInMg { get; set; }
     }
 }
