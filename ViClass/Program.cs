acf06524619df61fc202840ba2e33109da3dcb81
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-24 03:53:17 +0330

    Create required directories if are not exist in the Program class.

--- ViClass/Program.cs
+++ ViClass/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
@@ -11,13 +12,25 @@ namespace ViClass
 {
     public class Program
     {
+        private static string CurrentDirectory { get; } = Directory.GetCurrentDirectory();
+
         public static void Main(string[] args)
         {
+            CreateReacquiredDirectories();
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
+                                                                                    .UseStartup<Startup>();
+
+        private static void CreateReacquiredDirectories()
+        {
+            var classSharedFilesPath = Path.Combine(CurrentDirectory, "wwwroot", "Class Shared Files");
+            var classVideosPath      = Path.Combine(CurrentDirectory, "wwwroot", "Class Videos");
+            var profileImagePath     = Path.Combine(CurrentDirectory, "wwwroot", "Profile Images");
+            if (!Directory.Exists(classSharedFilesPath)) Directory.CreateDirectory(classSharedFilesPath);
+            if (!Directory.Exists(classVideosPath)) Directory.CreateDirectory(classVideosPath);
+            if (!Directory.Exists(profileImagePath)) Directory.CreateDirectory(profileImagePath);
+        }
     }
 }
