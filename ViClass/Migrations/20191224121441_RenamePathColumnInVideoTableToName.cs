b79d6bad095f3794a130b852ef24a6022b492f21
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-25 07:54:42 +0330

    Migration files for renaming a model property.

---
+++ ViClass/Migrations/20191224121441_RenamePathColumnInVideoTableToName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class RenamePathColumnInVideoTableToName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "Video",
+                newName: "SavedName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SavedName",
+                table: "Video",
+                newName: "Path");
+        }
+    }
+}
