5c2ca6989a90851123cb7aff00a05ac6ad71b807
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-26 10:06:14 +0330

    Change properties in each of models SharedFIle and Video.

--- ViClass/Controllers/Resources/VideoResource.cs
+++ ViClass/Controllers/Resources/VideoResource.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Description { get; set; }
         public string SavedName { get; set; }
-        public uint VolumeInMg { get; set; }
-        public string LengthFormatted { get; set; }
+        public string VolumeInMg { get; set; }
     }
 }
--- ViClass/Models/Video.cs
+++ ViClass/Models/Video.cs
@@ -2,12 +2,11 @@
 {
     public class Video
     {
-        public int Id { get; set; }
+        public int    Id          { get; set; }
         public string Description { get; set; }
-        public string SavedName { get; set; }
-        public uint VolumeInMg { get; set; }
-        public string LengthFormatted { get; set; }
-        public int ClassId { get; set; }
-        public Class Class { get; set; }
+        public string SavedName   { get; set; }
+        public string   VolumeInMg  { get; set; }
+        public int    ClassId     { get; set; }
+        public Class  Class       { get; set; }
     }
 }
