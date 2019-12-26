bf60848f4e0c5428ddf62931adaac0ef436e4953
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-26 11:15:12 +0330

    Update loading component to work without display and display setter.
    with null parameters will display permanently.

--- ViClass/ClientApp/src/components/Loading.jsx
+++ ViClass/ClientApp/src/components/Loading.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import ModalDialog from "./ModalDialog";
 import "../styles/loading.scss";
 
 function Loading({displayLoading, setDisplayLoading}) {
+    const [displayLoadingAlt, setDisplayLoadingAlt] = useState(true);
+    
     return (
         <ModalDialog modalId="loading-modal"
-                     visible={displayLoading}
-                     setVisibility={setDisplayLoading}
+                     visible={displayLoading!==null ?displayLoading: displayLoadingAlt}
+                     setVisibility={setDisplayLoading!==null ?setDisplayLoading: setDisplayLoadingAlt}
                      canCloseModal={false}>
             <div className="loading">
                 <div className="loading-wrapper">
