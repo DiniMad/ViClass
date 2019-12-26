e6c62ec73b3b219b576b1a6d1695c5b72cfb7fce
Seyed Hossein Hosseini <payoff900@gmail.com>
2019-12-26 10:44:24 +0330

    Migrations file for renaming a property in VIdeo model.

---
+++ ViClass/Migrations/20191226071003_RanameVolumeInMbColumnInVideoTableToVolumeInByte.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ViClass.Migrations
+{
+    public partial class RanameVolumeInMbColumnInVideoTableToVolumeInByte : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VolumeInMg",
+                table: "Video",
+                newName: "VolumeInByte");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VolumeInByte",
+                table: "Video",
+                newName: "VolumeInMg");
+        }
+    }
+}
